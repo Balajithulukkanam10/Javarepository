8d23dbeb26e3fdaa4faf903c158c1ce04b327d55
Balajithulukkanam10 <Balaji.thulukkanam10@gmail.com>
2024-04-12 20:03:31 +0530

    Added day wise topics

--- src/main/java/mainpackage/PublicClass.java
+++ src/main/java/mainpackage/PublicClass.java
@@ -1,5 +1,24 @@
 package mainpackage;
 
-public class PublicClass {
 
-}
+public class PublicClass {
+	int a = 5;
+	static int b = 0;
+	public static void main(String[] args) {
+		PublicClass obj = new PublicClass();
+		System.out.println(obj.a);
+		int a = 10;
+		System.out.println(a);
+		
+		System.out.println(b);
+		
+		int c = 20;
+	}
+	
+	public void method1() {
+//		System.out.println(c);
+	}
+	
+		
+	}
+
