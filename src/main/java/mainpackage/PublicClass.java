7b1ab41557afd3589c5b381d30c8c8c3db15a667
Balajithulukkanam10 <Balaji.thulukkanam10@gmail.com>
2024-04-16 21:17:49 +0530

    Balaji Commit

--- src/main/java/mainpackage/PublicClass.java
+++ src/main/java/mainpackage/PublicClass.java
@@ -1,24 +1,30 @@
 package mainpackage;
 
-
 public class PublicClass {
+	
 	int a = 5;
-	static int b = 0;
+	static int b = 50;
 	public static void main(String[] args) {
 		PublicClass obj = new PublicClass();
 		System.out.println(obj.a);
 		int a = 10;
+		int b = 20;
 		System.out.println(a);
-		
 		System.out.println(b);
-		
-		int c = 20;
-	}
-	
-	public void method1() {
-//		System.out.println(c);
-	}
-	
-		
+		obj.method1();
+//		int c = 20;
 	}
 
+	public void method1() {
+		int c = 9;
+		System.out.println(c);
+		System.out.println(b);
+	}
+	public void method() {
+		int c =10;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+		
+	}
