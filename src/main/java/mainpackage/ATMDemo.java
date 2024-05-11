4b99c63fdc63ef594b4358e2f71de51ddaaa3a0e
Balajithulukkanam10 <Balaji.thulukkanam10@gmail.com>
2024-05-11 17:52:08 +0530

    Balaji Commit may 11

---
+++ src/main/java/mainpackage/ATMDemo.java
@@ -0,0 +1,16 @@
+package mainpackage;
+
+public class ATMDemo {
+	public int metha() {
+
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		return c;
+		
+	}
+	public static void main(String[] args) {
+		ATMDemo obj = new ATMDemo();
+		obj.metha();
+	}
+}
---
+++ src/main/java/mainpackage/LoopConcepts.java
@@ -0,0 +1,14 @@
+package mainpackage;
+
+public class LoopConcepts {
+
+	public static void main(String[] args) {
+		
+//	for (i = 0; i<10; i++) {
+//			System.out.println("A");
+//		}
+		for (int i = 0; i<10; i++) {
+			System.out.println(i);
+		}
+	}
+}
